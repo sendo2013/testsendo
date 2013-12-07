d97bdecf97c0b97964225e2f67afe5ad7ea95f9c
mythndevil <mythndevil@nate.com>
2013-12-08 00:39:36 +0900

    first commit

---
+++ TestCode/src/Mysql/Mysql.java
@@ -0,0 +1,73 @@
+package Mysql;
+
+
+
+interface DBPool {
+	public MysqlDBHandler getHandler();
+	public boolean returnHandler(MysqlDBHandler returnHandler);
+	public boolean close();
+}
+
+
+interface CommonDBHandler {
+	public boolean createDB(String createDBName);
+	public boolean selectDB(String selectDBName);
+	public boolean dropDB(String dropDBName);
+	public boolean delete(String deleteKey);
+}
+
+interface MysqlDBHandler extends CommonDBHandler {
+	public boolean insertDB();
+	public String selectDB();
+}
+
+
+class MysqlPool implements DBPool {
+	
+	public MysqlDBHandler getHandler(){
+		return;
+	}
+	
+	
+	public boolean returnHandler(MysqlDBHandler returnHandler){
+		return true;
+	}
+	public boolean close(){
+		return true;
+	}
+}
+
+class MysqlHandler implements MysqlDBHandler {
+	public boolean createDB(String createDBName){
+		return true;
+	}
+	public boolean selectDB(String selectDBName){
+		return true;
+	}
+	public boolean dropDB(String dropDBName){
+		return true;
+	}
+	public boolean delete(String deleteKey){
+		return true;
+	}
+	
+	public boolean insertDB(){
+		return true;
+	}
+	public String selectDB(){
+		String sql = "";
+		return sql;
+	}
+}
+
+public class Mysql {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
